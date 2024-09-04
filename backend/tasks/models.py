31070d72010d34aaa49a57dd93a814cadcfe6106
MiguelAngeloTr <miguelmax248@gmail.com>
2024-09-04 05:47:53 -0500

    migrationspracticar

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarea(models.Model):
+    nombre= models.TextField(max_length=200)
+    description=models.TextField(null=True, blank=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+
+
+
+    
